66cd72a4082dbc333fe524698bbe798e72a76459
Ayush <74857388+worldofesports@users.noreply.github.com>
2021-07-02 16:36:21 +0530

    Update l.py

--- l.py
+++ l.py
@@ -1,2 +1,9 @@
 import pywhatkit
-pywhatkit.sendwhatmsg('+919438052022','Happy Birthday',12,3)
+pywhatkit.sendwhatmsg('your phone number with country code','your messages',hour,minute) #enter 24 hour time format 
+
+
+#time must me integer
+#minute must be integer
+#phonenumber must have +symbol and country code
+#phonenumber must be string not integer
+
